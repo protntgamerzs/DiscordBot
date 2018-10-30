7bec4e70233c9c1de7ef8ec777e871393e8ef380
protntgamerzs <35900983+protntgamerzs@users.noreply.github.com>
2018-10-30 17:49:53 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,27 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
-});
-
-client.on('message', message => {
-    if (message.content === 'cmds') {
-    	message.reply("commands are : \n"
-        "buy\n"
-        "gae\n");
-  	}
-});
-
-client.on('message', message => {
-    if (message.content === 'buy') {
-    	message.reply("https://selly.gg/p/fe31a887");
-  	}
-});
-        
-client.on('message', message => {
-    if (message.content === 'gae') {
-    	message.reply("You Are Gae");
-  	}
-});        
+});      
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
