22967a274edf9f1c8ce59326a28f0880a93f70a0
protntgamerzs <35900983+protntgamerzs@users.noreply.github.com>
2018-10-30 16:39:11 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === 'ping') {
-    	message.reply('Bro YOU GONNA SUCK MY DICK RN');
+    	message.reply('');
   	}
 });
 
