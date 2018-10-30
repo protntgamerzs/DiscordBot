b3a3fb74eb6d6c27ce2a059ae057f8a1f8bfa768
protntgamerzs <35900983+protntgamerzs@users.noreply.github.com>
2018-10-30 16:11:56 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'buy') {
-    	message.reply('pong'
+    if (message.content === 'ping') {
+    	message.reply('pong');
   	}
 });
 
