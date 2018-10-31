79ce33c14d4a78643ac6a1c31d636dc324b3f1c9
protntgamerzs <35900983+protntgamerzs@users.noreply.github.com>
2018-10-31 18:51:48 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,13 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const bot = new Discord.Client();
 
-bot.on("ready", () => {
+bot.on('ready', () => {
     console.log("Ready");
 });
 
 const prefix = "!";
 bot.on("message", (message) => {
-  if (message.content.startWith(prefix)) return;
+  if (!message.content.startWith(prefix)) return;
    
     if(message.content.startWith(prefix + "help")) {
         message.channel.send("OOF I CANT HELP YOU");
