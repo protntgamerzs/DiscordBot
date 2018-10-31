64d38e162572a0c416e535f59cf744752417df96
protntgamerzs <35900983+protntgamerzs@users.noreply.github.com>
2018-10-31 19:11:51 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,12 +7,20 @@ bot.on("message", (message) => {
   // Exit and stop if the prefix is not there or if user is a bot
   if (!message.content.startsWith(prefix) || message.author.bot) return;
  
-  if (message.content.startsWith(prefix + "hello")) {
-    message.channel.send("FUCK OFF!");
-  } else
-  if (message.content.startsWith(prefix + "why")) {
-    message.channel.send("ARE YOU DUMB!");
-  }
-});
+  if (message.content.startsWith(prefix + "info")) {
+    const info = new Discord.RichEmbed()
+    .setTitle("You can Buy ProSploit Here")
+    .setAuthor("ProSploit", "https://i.imgur.com/lm8s41J.png")
+    .setColor(0x00AE86)
+    .setFooter("Message From BotSploit", "http://i.imgur.com/CdYHQbi.png")
+    .setImage("http://i.imgur.com/CdYHQbi.png")                         
+    .setThumbnail("http://i.imgur.com/bZoA012.png") 
+    .setTimestamp()
+    .setURL("https://selly.gg/p/fe31a887")
+    .addBlankField(true)
+    .addField("Supports:",
+    "Full lua execution", "HttpGet", "Fast Injection")
  
-bot.login("NDUyNDYzMjUxNDk4ODYwNTk2.Drn6qA.yznePHjwvON9J5FtfVTpeCLCjfI");
+  message.channel.send({info});
+}});
+bot.login("NDUyNDYzMjUxNDk4ODYwNTk2.Drn6qA.yznePHjwvON9J5FtfVTpeCLCjfI"); 
