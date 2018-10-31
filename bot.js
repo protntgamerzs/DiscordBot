346bf69de00996878acedd34c60d1495ff6166a2
protntgamerzs <35900983+protntgamerzs@users.noreply.github.com>
2018-10-31 18:44:50 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,20 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client()
 
-bot.on("ready", function() {
+bot.on("ready", () => {
     console.log("Ready");
-
 });
+
 const prefix = "!";
 bot.on("message", (message) => {
   if (message.content.startWith(prefix)) return;
    
-    if(message.content.startWith("Help")) {
+    if(message.content.startWith(prefix + "help")) {
         message.channel.send("OOF I CANT HELP YOU");
-}
-});
+    } else
 
+    if(message.console.startWith(prefix + "pls")) {
+        message.channel.send("I SAID I CANT YOU MOTHER FUCKER!");    
+    }
+});
 bot.login(BOT_TOKEN);
