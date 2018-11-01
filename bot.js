fb9f1f581736ed11f5fdec91e985cdf6ca5d03b5
protntgamerzs <35900983+protntgamerzs@users.noreply.github.com>
2018-11-01 18:52:49 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,60 +1,32 @@
 const Discord = require('discord.js');
-const superagent = require("superagent");
-const Client = new Discord.Client();
-const OwnerID = "228066680268390400";
+const botConfig = require("./botconfig.json");
+const bot = new Discord.Client();
 
-const prefix = "/"
+bot.on("ready", async () => {
 
+	console.log(`${bot.user.username} Is Online!`);
 
-
-Client.on("ready", () => {
-	console.log("online");
-	Client.user.setPresence({ game: { name: `Hello world`, type: 0} });
-});
-
-Client.on("message", async (message) => {
-	if (message.author.bot) return;
-	if (!message.content.startsWith(prefix)) return;
+	bot.user.setActivity("with Gae", {type: "PLAYING"});
 	
-	let command = message.content.split(" ")[0];
-	command = command.slice(prefix.length);
+
+	//bot send message back
+	if (Message.author.bot) return;
+
+	if(Message.channel.type === "dm") return;
+
+	var prefix = botConfig.prefix;
 	
-	let args = message.content.split(" ").slice(1);
+	var messageArray = message.content.split("");
+
+	var command = messageArray[0];
+
+	var arguments = messageArray.slice(1);
+
+	if( command === `${prefix}Hi`){
+
+		return Message.Channel.send("Hey You are Gae");
 	
-	if (command === "ping") {
-		message.channel.send(`Gae! Time took: ${Date.now() - message.createdTimestamp} ms`);
-	} else
-
-	if (command === "say") {
-		message.delete()
-        const embed = new Discord.RichEmbed()
-		.setColor(0x954D23)
-		.setDescription(message.author.username + " says: " + args.join(" "));
-		message.channel.send({embed})
-	} else
-
-	if (command == "help") {
-		const embed = new Discord.RichEmbed()
-		.setColor(0x954D23)
-		.setTitle("Command List:")
-		.addField("!help", "Will give the current command list")
-		.addField("!ping", "WIll show the ping time for the bot")
-		.addField("!say [text]", "Will make the bot say something")
-		.addField("!buy", "To buy the exploit");
-		message.channel.send({embed})
-	} else
-
-	if (command == "buy") {
-		const embed = new Discord.RichEmbed()
-		.setColor(0x954D23)
-		.setTitle("ProSploit")
-		.addField("Supports:")
-		.addField("Full lua execution")
-		.addField("loadstrings")
-		.addField("fast injection");
-		message.channel.send({embed})
-	}
+	}	
 
 });
-
-Client.login(BOT_TOKEN);
+bot.login(BOT_TOKEN);
