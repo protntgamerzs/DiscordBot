e2403eff9cca5b1b6fadc869a3167e275e2b9ec1
protntgamerzs <35900983+protntgamerzs@users.noreply.github.com>
2018-11-01 15:25:37 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,18 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
  
-client.on("ready", () => {
-  console.log("I am ready!");
-});
- 
+// Set the prefix
+let prefix = "!";
 client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
+  // Exit and stop if the prefix is not there or if user is a bot
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+ 
+  if (message.content.startsWith(prefix + "help")) {
+    message.channel.send("FUCK OFF! IF YOU SAY PLS I KILL YOU");
+  } else
+  if (message.content.startsWith(prefix + "PLS")) {
+    message.channel.send("SHOOTS 🔫");
   }
 });
- 
-client.login("SuperSecretBotTokenHere");
+
+client.login(procces.env.BOT_TOKEN);
