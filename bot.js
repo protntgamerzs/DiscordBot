e44b2b2e47f2aa9281b6d5f14dc14de2e94c156f
protntgamerzs <35900983+protntgamerzs@users.noreply.github.com>
2018-11-01 19:06:53 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
  
 // Set the prefix
-let prefix = "!";
+let prefix = "/";
 client.on("message", (message) => {
   // Exit and stop if the prefix is not there or if user is a bot
   if (!message.content.startsWith(prefix) || message.author.bot) return;
