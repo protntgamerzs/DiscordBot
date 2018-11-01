6eade86307adb1c54ba07fca5f62fd1657ce4691
protntgamerzs <35900983+protntgamerzs@users.noreply.github.com>
2018-11-01 16:16:57 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,18 +1,72 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
- 
-// Set the prefix
-let prefix = "!";
-client.on("message", (message) => {
-  // Exit and stop if the prefix is not there or if user is a bot
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
- 
-  if (message.content.startsWith(prefix + "help")) {
-    message.channel.send("no!");
-  } else
-  if (message.content.startsWith(prefix + "pls")) {
-    message.channel.send("YOU ARE SO STUPID SPOOKYGAMES MADE ME FOR BANNING PEOPLE NOT FOR HELPING!");
-  }
+const Discord = require('discord.js');
+const superagent = require("superagent");
+const Client = new Discord.Client();
+const OwnerID = "228066680268390400";
+
+const prefix = "!"
+
+
+
+Client.on("ready", () => {
+	console.log("online");
+	Client.user.setPresence({ game: { name: `Hello world`, type: 0} });
 });
- 
-client.run(BOT_TOKEN);
+
+// welcome message
+
+Client.on("guildMemberAdd", member => {
+   member.guild.defaultChannel.send("Welcome to: " + member.guild.name + " Hope you enjoy it here")
+});
+
+Client.on("guildMemberRemove", member => {
+   member.guild.defaultChannel.send("Goodbye: " + member.user.username + " from " + member.guild.name)
+});
+
+Client.on("message", async (message) => {
+	if (message.author.bot) return;
+	if (!message.content.startsWith(prefix)) return;
+	
+	let command = message.content.split(" ")[0];
+	command = command.slice(prefix.length);
+	
+	let args = message.content.split(" ").slice(1);
+	
+	if (command === "ping") {
+		message.channel.send(`Pong! Time took: ${Date.now() - message.createdTimestamp} ms`);
+	} else
+
+	if (command === "say") {
+		message.delete()
+        const embed = new Discord.RichEmbed()
+		.setColor(0x954D23)
+		.setDescription(message.author.username + " says: " + args.join(" "));
+		message.channel.send({embed})
+	} else
+
+	if (command == "help") {
+		const embed = new Discord.RichEmbed()
+		.setColor(0x954D23)
+		.setTitle("Command List:")
+		.addField("!help", "Will give the current command list")
+		.addField("!ping", "WIll show the ping time for the bot")
+		.addField("!say [text]", "Will make the bot say something")
+  .addField("!buy", "To buy the exploit")
+		message.channel.send({embed})
+	} else
+  
+  if (command == "buy") {
+		const embed = new Discord.RichEmbed()
+		.setColor(0x954D23)
+		.setTitle("Synthex")
+  .setField("Supports:")
+		.addField("Full Lua Execution")
+		.addField("Loadstrings")
+		.addField("Fast injection")
+		message.channel.send({embed})
+	}
+
+});
+
+Client.login(BOT_TOKEN);
+
+
